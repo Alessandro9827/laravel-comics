b7d1073149486784f238195dd625d1eb7c26336d
Alessandro9827 <ale.moramarco27@gmail.com>
2024-02-01 20:16:45 +0100

    added product in web /product

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
-
-
-Route::get('/products', function () {
-    $pastas = config('db.pasta');
-    return view('pages.products', [ 'products' => $pastas] );
+Route::get('/comics', function () {
+    $comics = config('db.comic');
+    return view('pages.comics', [ 'comics' => $comics]);
 });
+
